2532f81bc602c6d60a6d5c3a77fb91b7c37e7216
denniscosta13 <dennismeira.costa@gmail.com>
2023-09-14 14:17:04 -0300

    AI now working, project done

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import cors from "cors"
 import express from "express"
 
+import { convert } from "./convert.js"
 import { download } from "./download.js"
 import { transcribe } from "./transcribe.js"
 import { summarize } from "./summarize.js"
@@ -10,18 +11,30 @@ app.use(express.json())
 app.use(cors())
 
 app.get("/summary/:id", async (request, response) => {
-  const { id } = request.params
+  try {
+    const { id } = request.params
 
-  await download(id)
-  const result = await transcribe()
+    await download(id)
+    const audioConverterd = await convert()
 
-  response.json({ result })
+    const result = await transcribe(audioConverterd)
+
+    response.json({ result })
+  } catch (error) {
+    console.log(error)
+    response.json({ error })
+  }
 })
 
 app.post("/summary", async (request, response) => {
-  const result = await summarize(request.body.text)
+  try {
+    const result = await summarize(request.body.text)
 
-  return response.json({ result })
+    return response.json({ result })
+  } catch (error) {
+    console.log(error)
+    response.json({ error })
+  }
 })
 
 app.listen(3333, () => console.log("Server is running on port 3333"))
